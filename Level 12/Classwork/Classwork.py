8a6d7e45f4250d3d392af9e5c11525e6dadabb56
chumbualex <chumburidze9876@gmail.com>
2024-12-02 16:31:34 +0400

    commit

---
+++ Level 12/Classwork/Classwork.py
@@ -0,0 +1,44 @@
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+print(True or False)
+
+
+print(True and False)
+
+print(True and False)
+
+print(True and False)
+
+print(True and False)
+
+print(True and False)
+
+print(True and False)
+
+print(True and False)
+
+print(True and False)
+
+
+print(True and False)
+
+
+
+print(True and False)
+
